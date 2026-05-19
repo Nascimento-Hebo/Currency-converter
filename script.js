b9f62d08f919ac6ce80ce7363c607e2f12b3831f
Nascimento-Hebo <nascimentohebo2@gmail.com>
2026-05-19 16:50:33 -0300

    estou fazendo uma melhoria interna

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('.Convert-button');
+button.addEventListener('click', convertCurrency);
+
+function convertCurrency() {
+    const currencyFrom = document.getElementById('#currency-from');
+    const currencyTo = document.getElementById('#currency-to');
+    const amount = document.querySelector('.currency-from').value;
+
+
+}
