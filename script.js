736a9f24ddd1f15e500ea4097e43966d91ddb399
Nascimento-Hebo <nascimentohebo2@gmail.com>
2026-05-22 16:39:25 -0300

    feat:adicionei uma nova funcionalidade no javascript

--- script.js
+++ script.js
@@ -4,18 +4,38 @@ function convertCurrency() {
     const input = document.querySelector('.currency-input').value;
     const ParagraphValue = document.querySelector('.value-placed');
     const ParagraphResult = document.querySelector('.value-result');
+    const select = document.querySelector('#currency-from').value;
+    console.log(select)
 
-    ParagraphValue.innerHTML = new Intl.NumberFormat('pt-PT', {
+    const ValorEmDollar = 5.25;
+    const ValorEmReal = 182.29;
+    const ValorEmEuro = 1000;
+
+    if (select == 'USD') {
+        ParagraphResult.innerHTML = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD'
+        }).format(input / ValorEmDollar);
+    }
+
+    if (select == 'EUR') {
+        ParagraphResult.innerHTML = new Intl.NumberFormat('de-DE', {
+            style: 'currency',
+            currency: 'EUR'
+        }).format(input / ValorEmEuro);
+    }
+
+    if (select == 'BRL') {
+        ParagraphResult.innerHTML = new Intl.NumberFormat('pt-br', {
+            style: 'currency',
+            currency: 'BRL'
+        }).format(input / ValorEmReal);
+    }
+
+    ParagraphValue.innerHTML = new Intl.NumberFormat('pt-AO', {
         style: 'currency',
         currency: 'AOA'
-    }).format(input);
+    }).format(input)
 
-    ParagraphResult.innerHTML = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD'
-    }).format(convertCurrency);
-
-    const ValllorEmDollar = 5.25;
-    convertCurrency = input / ValllorEmDollar;
 }
 button.addEventListener('click', convertCurrency);
